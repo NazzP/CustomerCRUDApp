0646f853028305e6fe118179ce5b09c96bd93cf8
Nazz <nazz.panasiuk@gmail.com>
2024-06-21 22:10:04 +0300

    Entity Validation impl
    1.  fullName: String (2..50 chars including whitespaces)
    2.  email: String (2..100 chars, unique, should include exactly one @)
    3. phone: String (6..14 chars, only digits, should start from +, optional field)

--- src/main/java/org/example/customercrudapp/entity/Customer.java
+++ src/main/java/org/example/customercrudapp/entity/Customer.java
@@ -1,13 +1,16 @@
 package org.example.customercrudapp.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
@@ -23,12 +26,18 @@ public class Customer {
     @Column(name = "id", nullable = false, updatable = false)
     private Long id;
 
+    @NotBlank(message = "Full name is required")
+    @Size(min = 2, max = 50, message = "Full name must be between 2 and 50 characters")
     @Column(name = "full_name", nullable = false, length = 50)
     private String fullName;
 
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    @Size(min = 2, max = 100, message = "Email must be between 2 and 100 characters")
     @Column(name = "email", nullable = false, unique = true, length = 100)
     private String email;
 
+    @Pattern(regexp = "\\+\\d{6,14}", message = "Phone number must start with '+' and contain only digits, and be between 6 and 14 characters")
     @Column(name = "phone", nullable = true, length = 14)
     private String phone;
 
